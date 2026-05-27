f0b4eb19c2989e698be8dbc0c7087387c0939a65
Marcelo Rodrigues <85708828+marcelo-mmdev@users.noreply.github.com>
2026-05-26 23:06:50 -0300

    Feat: Inicio dos estudos em java basico

---
+++ Aula/aula/src/main/java/backend/Main.java
@@ -0,0 +1,30 @@
+package backend;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Estudos!");
+        /*
+        * Modificadores de acessos.
+        *   public, protected e private
+        *
+        * Variaveis + tipos de dados(int, float, boolean, char, String)
+        * Estruturas condicionais( if, else, else if, switch)
+        * Estruturas de repetição(for, while, do while)
+        * Blocos de codigos(metodos)
+        * Orientação a Objetos(POO)
+        *   - Classes(concreta e abstrata)
+        *   - Objetos
+        *   - Atributos + tipos de dados
+        *   - Construtores(com e sem parametros)
+        *   - Herança
+        *   - Polimorfismo(sobrecarga e sobreescrita)
+        *   - Associação(composição, agregação)
+        *   - Interface
+        *   - Classes e Metodos abstratos(Abstração)
+        *   - Coleções(list(arrayList), set(hashSet), map(hashMap))
+        *   - Pacotes(Arquitetura do projeto)
+        *
+        * */
+
+    }
+}
